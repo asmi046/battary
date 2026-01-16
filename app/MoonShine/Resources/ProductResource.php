584f6a1c2549fcdd95820bd4d84c6bf54a8d15c3
asmi046 <asmi046@gmail.com>
2026-01-16 14:42:56 +0300

    Правки по редактированию товара

--- app/MoonShine/Resources/ProductResource.php
+++ app/MoonShine/Resources/ProductResource.php
@@ -5,28 +5,25 @@ declare(strict_types=1);
 namespace App\MoonShine\Resources;
 
 use App\Models\Product;
-use MoonShine\Fields\ID;
-
-use MoonShine\Fields\Json;
-use MoonShine\Fields\Slug;
-use MoonShine\Fields\Text;
-use MoonShine\Fields\Field;
-use MoonShine\Fields\Image;
-use MoonShine\Fields\Number;
-use MoonShine\Fields\Select;
-use MoonShine\Decorations\Tab;
-use MoonShine\Fields\Position;
-use MoonShine\Fields\Switcher;
-use MoonShine\Decorations\Tabs;
-use MoonShine\Handlers\ExportHandler;
-use MoonShine\Handlers\ImportHandler;
-use MoonShine\Resources\ModelResource;
 use Illuminate\Database\Eloquent\Model;
 use MoonShine\ActionButtons\ActionButton;
 use MoonShine\Components\MoonShineComponent;
-use App\MoonShine\Resources\CategoryResource;
-use MoonShine\Tests\Fixtures\Models\Category;
+use MoonShine\Decorations\Tab;
+use MoonShine\Decorations\Tabs;
+use MoonShine\Fields\Field;
+use MoonShine\Fields\ID;
+use MoonShine\Fields\Image;
+use MoonShine\Fields\Json;
+use MoonShine\Fields\Number;
+use MoonShine\Fields\Position;
 use MoonShine\Fields\Relationships\BelongsToMany;
+use MoonShine\Fields\Select;
+use MoonShine\Fields\Slug;
+use MoonShine\Fields\Switcher;
+use MoonShine\Fields\Text;
+use MoonShine\Handlers\ExportHandler;
+use MoonShine\Handlers\ImportHandler;
+use MoonShine\Resources\ModelResource;
 
 /**
  * @extends ModelResource<Product>
@@ -57,16 +54,15 @@ class ProductResource extends ModelResource
             Text::make('Бренд', 'brand'),
             Text::make('Описание SEO', 'seo_description'),
             Select::make('Тип', 'type')
-                    ->options([
-                        'Аккумуляторы для грузовых автомобилей' => 'Аккумуляторы для грузовых автомобилей',
-                        'Аккумуляторы для легковых автомобилей' => 'Аккумуляторы для легковых автомобилей',
-                        'Аккумуляторы для мототехники' => 'Аккумуляторы для мототехники',
-                        'Аккумуляторы для спецтехники' => 'Аккумуляторы для спецтехники',
-                        'Тяговые аккумуляторы' => 'Тяговые аккумуляторы',
-                    ])->nullable()
+                ->options([
+                    'Аккумуляторы для грузовых автомобилей' => 'Аккумуляторы для грузовых автомобилей',
+                    'Аккумуляторы для легковых автомобилей' => 'Аккумуляторы для легковых автомобилей',
+                    'Аккумуляторы для мототехники' => 'Аккумуляторы для мототехники',
+                    'Аккумуляторы для спецтехники' => 'Аккумуляторы для спецтехники',
+                    'Тяговые аккумуляторы' => 'Тяговые аккумуляторы',
+                ])->nullable(),
         ];
     }
-
 
     /**
      * @return list<Field>
@@ -94,7 +90,7 @@ class ProductResource extends ModelResource
                 Tab::make('Товар', [
                     ID::make()->sortable(),
                     Switcher::make('Показывать товар', 'show'),
-                    Text::make('Название', 'name')->required(),
+                    Text::make('Название', 'name')->required()->unescape(),
                     Slug::make('Ссылка', 'slug')->from('name'),
                     Text::make('Артикул', 'sku')->required(),
                     Text::make('Бренд', 'brand'),
@@ -110,44 +106,42 @@ class ProductResource extends ModelResource
                     Text::make('Ширина', 'width'),
                     Text::make('Длинна', 'length'),
                     Select::make('Расположение клемм', 'clem_location')
-                    ->options([
-                        1 => 'Прямая',
-                        0 => 'Обратная',
-                    ])->nullable(),
+                        ->options([
+                            1 => 'Прямая',
+                            0 => 'Обратная',
+                        ])->nullable(),
 
                     Text::make('Вольтаж', 'voltage'),
                     Switcher::make('Популярный товар', 'popular'),
                     Select::make('Тип', 'type')
-                    ->options([
-                        'Аккумуляторы для грузовых автомобилей' => 'Аккумуляторы для грузовых автомобилей',
-                        'Аккумуляторы для легковых автомобилей' => 'Аккумуляторы для легковых автомобилей',
-                        'Аккумуляторы для мототехники' => 'Аккумуляторы для мототехники',
-                        'Аккумуляторы для спецтехники' => 'Аккумуляторы для спецтехники',
-                        'Тяговые аккумуляторы' => 'Тяговые аккумуляторы',
-                    ])->nullable()
+                        ->options([
+                            'Аккумуляторы для грузовых автомобилей' => 'Аккумуляторы для грузовых автомобилей',
+                            'Аккумуляторы для легковых автомобилей' => 'Аккумуляторы для легковых автомобилей',
+                            'Аккумуляторы для мототехники' => 'Аккумуляторы для мототехники',
+                            'Аккумуляторы для спецтехники' => 'Аккумуляторы для спецтехники',
+                            'Тяговые аккумуляторы' => 'Тяговые аккумуляторы',
+                        ])->nullable(),
                 ]),
                 Tab::make('Галерея товара', [
                     Json::make('Галерея изображений', 'galery')
+                        ->removable()
                         ->fields([
                             Position::make(),
                             Text::make('Заголовок', 'title'),
                             Image::make('Изображения', 'img')->dir('products'),
-                        ])
+                        ]),
                 ]),
                 Tab::make('Категории товара', [
-                    BelongsToMany::make('Категории товара', 'category_tovars', resource: new CategoryResource())
-                ])
+                    BelongsToMany::make('Категории товара', 'category_tovars', resource: new CategoryResource),
+                ]),
             ]),
 
             ActionButton::make(
                 label: 'Просмотр страницы товара',
                 url: route('product_page', $this->getItem()['slug']),
             )
-            ->blank()
-            ->success()
-
-
-
+                ->blank()
+                ->success(),
 
         ];
     }
@@ -163,9 +157,9 @@ class ProductResource extends ModelResource
     }
 
     /**
-     * @param Product $item
-     *
+     * @param  Product  $item
      * @return array<string, string[]|string>
+     *
      * @see https://laravel.com/docs/validation#available-validation-rules
      */
     public function rules(Model $item): array
